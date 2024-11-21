3ede0c1483a4cab57e3f13116320a067079ee13a
Vinicius Tanaka <vi_massaru.tanaka@hotmail.com>
2024-11-21 02:19:23 -0300

    update: adicionando variaveis de ambiente

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 # flake8: noqa
 
 from pathlib import Path
-from dotenv import dotenv_values
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
