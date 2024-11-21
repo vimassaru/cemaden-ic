e5490b0633cd167b2a38675ee4e43504507baec4
Vinicius Tanaka <vi_massaru.tanaka@hotmail.com>
2024-11-21 03:26:09 -0300

    update: adicionando variaveis de ambiente

--- core/settings.py
+++ core/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config['DB_NAME'],
-        'USER': config['DB_USERNAME'],
-        'PASSWORD': config['DB_PASSWORD'],
-        'HOST': config['DB_HOST'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': config['DB_NAME'],
+#         'USER': config['DB_USERNAME'],
+#         'PASSWORD': config['DB_PASSWORD'],
+#         'HOST': config['DB_HOST'],
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
